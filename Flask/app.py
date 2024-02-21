92626c3fed51aea199669a9d8081663fa2bd9144
aaronjoju07 <aaronjoju07@gmail.com>
2024-02-21 13:05:27 +0530

    Advance Python

---
+++ Flask/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+# Define a simple endpoint
+@app.route('/api/hello', methods=['GET'])
+def hello():
+    response = {'message': 'Hello, this is a sample Flask API!'}
+    return jsonify(response)
+
+def about():
+    return 'This isn\'t about me!!'
+app.add_url_rule("/about","about",about)
+
+if __name__ == '__main__':
+    app.run(debug=True)
